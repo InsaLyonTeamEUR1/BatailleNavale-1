32c9518ce8f5b4f848170649d0a33780cd864082
ThomasG90 <tgirosiut90@gmail.com>
2014-12-03 11:34:19 +0100

    Create ControlListe2.java

---
+++ ControlListe2.java
@@ -0,0 +1,40 @@
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.*;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+public class ControlListe2 implements ActionListener {
+    Fenetre fen;
+    int i;
+    int j;
+    int compt;
+    public ControlListe2(Fenetre fen) {
+        this.fen = fen;
+    }
+    public void actionPerformed(ActionEvent ev){
+        fen.comptnombre=0;
+        System.out.println("Veuillez placer un " +fen.liste2.getSelectedItem());
+        for(int i=0;i<5;i++){
+            if(fen.liste2.getSelectedItem()==fen.items[i]) {
+                compt=i;
+                if(compt==0){
+                    fen.compFin=5;
+                }
+                if(compt==1){
+                    fen.compFin=4;
+                }
+                if(compt==2 || compt==3){
+                    fen.compFin=3;
+                }
+                if(compt==4 ){
+                    fen.compFin=2;
+                }
+            }
+        }
+    }
+}
